3d2633d7002b7c397bfd4416cf61c60e99b56b55
Ляхов Степан <lyakhov.sr@reliab.tech>
2021-04-15 21:03:48 +0300

    refactor: rename gruop to group

--- internal/domain/group.go
+++ internal/domain/group.go
@@ -4,25 +4,25 @@ import (
 	"fmt"
 )
 
-type Gruop struct {
+type Group struct {
 	title       string
 	description string
 	tasks       []Task
 }
 
-func (g *Gruop) getTasks() []Task {
+func (g *Group) getTasks() []Task {
 	return g.tasks
 }
 
-func (g *Gruop) getTitle() string {
+func (g *Group) getTitle() string {
 	return g.title
 }
 
-func (g *Gruop) getDescription() string {
+func (g *Group) getDescription() string {
 	return g.description
 }
 
-func (g *Gruop) findTaskByTitle(title string) (Task, error) {
+func (g *Group) findTaskByTitle(title string) (Task, error) {
 
 	for _, task := range g.tasks {
 		if task.title == title {
