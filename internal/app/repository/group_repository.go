ec338f385cb9772d8940a39eec574a819876218f
Ляхов Степан <lyakhov.sr@reliab.tech>
2021-04-18 19:37:19 +0300

    feat: add repository and mock db for group

---
+++ internal/app/repository/group_repository.go
@@ -0,0 +1,11 @@
+package repository
+
+import "domain"
+
+type GroupRepository interface {
+	Save(group *domain.Group) error
+	Update(group *domain.Group) error
+	GetByID(id domain.GroupID) (*domain.Group, error)
+	DelByID(id domain.GroupID) error
+	GetAll() []*domain.Group
+}
---
+++ internal/infrastructure/repository/in_memory_group.go
@@ -0,0 +1,53 @@
+package repository
+
+import (
+	"domain"
+	"errors"
+)
+
+type InMemoryGroup struct {
+	db map[domain.GroupID]*domain.Group
+}
+
+func (i InMemoryGroup) Save(group *domain.Group) error {
+	i.db[group.ID()] = group
+	return nil
+}
+
+func (i InMemoryGroup) Update(group *domain.Group) error {
+	_, err := i.db[group.ID()]
+	if err == false {
+		return errors.New("not found")
+	}
+
+	i.db[group.ID()] = group
+	return nil
+}
+
+func (i InMemoryGroup) GetByID(id domain.GroupID) (*domain.Group, error) {
+	_, err := i.db[id]
+	if err == false {
+		return &domain.Group{}, errors.New("not found")
+	}
+
+	return i.db[id], nil
+}
+
+func (i InMemoryGroup) DelByID(id domain.GroupID) error {
+	_, err := i.db[id]
+	if err == false {
+		return errors.New("not found")
+	}
+
+	delete(i.db, id)
+	return nil
+}
+
+func (i InMemoryGroup) GetAll() []*domain.Group {
+	var groups []*domain.Group
+
+	for _, group := range i.db {
+		groups = append(groups, group)
+	}
+	return groups
+}
