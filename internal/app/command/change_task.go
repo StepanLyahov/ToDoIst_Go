4c28649f628893bb4fac95aca75f558297be1f31
stepanlahov <lyahov.stepa@yandex.ru>
2021-04-27 23:08:53 +0300

    feat: created change_task

---
+++ internal/app/command/change_task.go
@@ -0,0 +1,40 @@
+package command
+
+import (
+	"app/query"
+	"app/repository"
+	"domain"
+)
+
+type ChangeTaskHandler struct {
+	taskRepos repository.TaskRepository
+}
+
+func NewChangeTaskHandler(taskRepos repository.TaskRepository) ChangeTaskHandler {
+	return ChangeTaskHandler{
+		taskRepos: taskRepos,
+	}
+}
+
+func (h ChangeTaskHandler) Execute(taskDto query.TaskDto) error {
+	taskID, err := domain.NewTaskIDFromString(taskDto.Id)
+	if err != nil {
+		return err
+	}
+
+	_, err = h.taskRepos.GetByID(taskID)
+	if err != nil {
+		return err
+	}
+
+	updatedTask, err := query.TaskToDomain(taskDto)
+	if err != nil {
+		return err
+	}
+
+	err = h.taskRepos.Update(&updatedTask)
+	if err != nil {
+		return err
+	}
+	return nil
+}
