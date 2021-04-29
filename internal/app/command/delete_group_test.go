eef40a711f5d3731a15e3e68360f3059fa20ade5
stepanlahov <lyahov.stepa@yandex.ru>
2021-04-29 13:49:56 +0300

    feat: add tests for delete_group

---
+++ internal/app/command/delete_group_test.go
@@ -0,0 +1,41 @@
+package command
+
+import (
+	"domain"
+	"infrastructure/repository"
+	"log"
+	"testing"
+)
+
+func TestDeleteGroup(t *testing.T) {
+	h := NewDeleteGroupHandler(repository.NewInMemoryGroup())
+
+	group := domain.NewGroup("", "")
+	log.Print(group.ID())
+
+	err := h.groupRepos.Save(group)
+	if err != nil {
+		t.Fatalf("It shouldn't be a problem, but Err := %v", err)
+	}
+
+	err = h.Execute(group.ID().String())
+	if err != nil {
+		t.Fatalf("Execute musted be del by ID, but Err := %v", err)
+	}
+
+	_, err = h.groupRepos.GetByID(group.ID())
+	if err == nil {
+		t.Fatalf("Err must be 'not found' but err is nil")
+	}
+
+}
+
+func TestDeleteGroupWithInvalidID(t *testing.T) {
+	h := NewDeleteGroupHandler(repository.NewInMemoryGroup())
+
+	err := h.Execute("invalid uuid")
+	if err == nil {
+		t.Fatalf("uuid must be invalid")
+	}
+
+}
