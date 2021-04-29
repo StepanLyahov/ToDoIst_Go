996613f997a15114b70988201c2298fa8540c1fc
stepanlahov <lyahov.stepa@yandex.ru>
2021-04-29 17:48:53 +0300

    feat: add test for add_new_task_to_group

---
+++ internal/app/command/add_new_task_to_group_test.go
@@ -0,0 +1,59 @@
+package command
+
+import (
+	"app/query"
+	"domain"
+	"infrastructure/repository"
+	"testing"
+)
+
+func initGroupHandlerBeforeTest(h *AddNewTaskToGroupHandler) domain.GroupID {
+	gDto := GroupDTO{
+		Title:       "Title",
+		Description: "Description",
+	}
+
+	group := domain.NewGroup(gDto.Title, gDto.Description)
+	err := h.groupRepos.Save(group)
+	if err != nil {
+		panic(err)
+	}
+
+	return group.ID()
+}
+
+func TestNewAddNewTaskToGroup(t *testing.T) {
+	h := NewAddNewTaskToGroupHandler(repository.NewInMemoryGroup(), repository.NewInMemoryTask())
+	groupId := initGroupHandlerBeforeTest(&h)
+
+	taskDto := query.TaskDto{
+		Title:       "Title",
+		Description: "Description",
+		Priority:    2,
+	}
+
+	taskID, err := h.Execute(groupId.String(), taskDto)
+	if err != nil {
+		t.Fatalf("Err must be nil, but err %v", err)
+	}
+
+	task, err := h.taskRepos.GetByID(taskID)
+	if err != nil {
+		t.Fatalf("Err must be nil, but err %v", err)
+	}
+
+	if !compareTask(taskDto, *task) {
+		t.Fatalf("Dto = '%v' and Group = '%v' does not compare",
+			taskDto, *task)
+	}
+}
+
+func compareTask(gDto query.TaskDto, task domain.Task) bool {
+	if gDto.Description != task.Description() {
+		return false
+	}
+	if gDto.Title != task.Title() {
+		return false
+	}
+	return true
+}
