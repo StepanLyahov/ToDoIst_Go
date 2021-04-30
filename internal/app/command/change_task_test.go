3bc6c94b84f3910191170d601cfc903f441a3d65
stepanlahov <lyahov.stepa@yandex.ru>
2021-04-30 12:56:41 +0300

    feat: created test for change_task

---
+++ internal/app/command/change_task_test.go
@@ -0,0 +1,43 @@
+package command
+
+import (
+	"app/query"
+	"domain"
+	"infrastructure/repository"
+	"testing"
+)
+
+func TestChangeTask(t *testing.T) {
+	title := "lol"
+
+	h := NewChangeTaskHandler(repository.NewInMemoryTask())
+	task := initTaskBeforeTest(&h)
+
+	dtoTask := query.TaskToDto(task)
+	dtoTask.Title = title
+
+	err := h.Execute(dtoTask)
+	if err != nil {
+		t.Fatalf("Execute must be ok, but err := %v", err)
+	}
+
+	updatedTask, err := h.taskRepos.GetByID(task.ID())
+	if err != nil {
+		t.Fatalf("In repository must be task by id := %v, but err := %v", task.ID().String(), err)
+	}
+
+	if updatedTask.Title() != title {
+		t.Fatalf("Title in updated task{%v} must be %v", updatedTask, title)
+	}
+
+}
+
+func initTaskBeforeTest(h *ChangeTaskHandler) domain.Task {
+	task := domain.NewTaskWithCurrentDate("t", "d", domain.Priority1)
+	err := h.taskRepos.Save(task)
+	if err != nil {
+		panic(err)
+	}
+
+	return *task
+}
